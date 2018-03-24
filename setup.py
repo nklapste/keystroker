692bc90bb50e32460fef0362553d5b32b0cd3907
Nathan Klapstein <nklapste@ualberta.ca>
2018-03-24 13:40:58 -0400

    updating `setup.py`

--- setup.py
+++ setup.py
@@ -1,40 +1,47 @@
-import os
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""setup.py for keystroker"""
 
 from setuptools import setup, find_packages
 
 
-PKG_DIR = os.path.dirname(os.path.realpath(__file__))
-
-
-def load_long_description():
-    """Read README.md for long_description"""
-    with open(os.path.join(PKG_DIR, "README.md"), "r") as readme:
-        return readme.read()
+def readme():
+    with open("README.rst") as f:
+        return f.read()
 
 
 setup(
     name="keystroker",
     version="0.0.0",
+    description="Send one or more keystroke or keystroke combinations "
+                "to the active window.",
+    long_description=readme(),
+    keywords="send keys keystroker key stroker windows",
 
     author="Nathan Douglas Klapstein",
     author_email="nklapste@ualberta.ca",
-    license="MIT",
-    description="Package that allows for direct windows "
-                "keystrokes to be sent onto executed PC",
-    long_description=load_long_description(),
-    keywords="send keys keystroker key stroker windows",
-    url="https://github.com/nklapste/keystroker",
-    download_url="",
-    classifiers="",
 
-    packages=find_packages(exclude=["test"]),
+    url="https://github.com/nklapste/keystroker",
+    download_url="https://github.com/nklapste/keystrokerarchive/0.0.0.tar.gz",  # TODO generate tag
+
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+
+    packages=find_packages(exclude=["test", "doc", "build"]),
     package_data={
-        '': ['LICENSE', 'README.md'],
-    },
-    entry_points={
-        'console_scripts': [
-            'keystroker = keystroker.__main__:main'
-        ]
+        "": ["LICENSE", "README.md"],
     },
     install_requires=[],
+    tests_requires=["pytest",],
+    entry_points={
+        "console_scripts": [
+            "keystroker = keystroker.__main__:main"
+        ]
+    },
 )
