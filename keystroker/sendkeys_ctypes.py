9f805402b50abdea62f9daa41c352de818120636
Nathan Klapstein <nklapste@ualberta.ca>
2018-03-24 10:58:57 -0400

    improved typing

--- keystroker/sendkeys_ctypes.py
+++ keystroker/sendkeys_ctypes.py
@@ -23,9 +23,9 @@ def _key_up(vk):
     windll.user32.keybd_event(vk, scan, KEYEVENTF_KEYUP, 0)
 
 
-def toggle_numlock(turn_on) -> int:
-    """Turns NUMLOCK on or off and returns whether it was originally on or off.
-    """
+def toggle_numlock(turn_on: bool) -> int:
+    """Turns `NUMLOCK` on or off and returns whether it was originally on
+    or off"""
     is_on = windll.user32.GetKeyState(VK_NUMLOCK) & 1
 
     if is_on != turn_on:
@@ -45,13 +45,13 @@ def toggle_numlock(turn_on) -> int:
     return is_on
 
 
-def char2keycode(char) -> int:
+def char2keycode(char: str) -> int:
     """Converts character to virtual key code"""
     vk = windll.user32.VkKeyScanA(ord(char))
     return vk
 
 
-def key_down(key) -> None:
+def key_down(key: int) -> None:
     """Generates a key pressed event
 
     :param key: a virtual key code
@@ -61,7 +61,7 @@ def key_down(key) -> None:
     _key_down(vk)
 
 
-def key_up(key) -> None:
+def key_up(key: int) -> None:
     """Generates a key released event
 
     :param key: a virtual key code
